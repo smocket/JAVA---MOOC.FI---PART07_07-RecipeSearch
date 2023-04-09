548f22f1eddadc857717c8762f7e1a030df5bda6
smoket <129101378+smocket@users.noreply.github.com>
2023-04-09 11:35:02 +0300

    Update RecipeSearch.java

--- RecipeSearch.java
+++ RecipeSearch.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 public class RecipeSearch {
     
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-        UserInterface ui = new UserInterface();
+        UserInterface ui = new UserInterface(scanner);
         ui.start();
     }
 }
